9490d8f23598ad18c92e9416c4d5a7a0adbb1cc6
saranthecoder <115061220+saranthecoder@users.noreply.github.com>
2024-07-25 07:07:52 +0530

    deploy update7

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -6,14 +6,12 @@ const generateTokenSetCookie = (userId, batchnumber, res) => {
     });
 
     res.cookie("jwt", token, {
-        maxAge: 1 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development",
+        maxAge: 1 * 60 * 60 * 1000, // 1 hour in milliseconds
+        httpOnly: true, // Makes the cookie accessible only through HTTP(S), not JavaScript
+        sameSite: "strict", // Ensures the cookie is sent only with requests from the same site
+        secure: process.env.NODE_ENV !== "development", // Sends the cookie only over HTTPS in production
+        domain: new URL(process.env.FRONTEND_URL).hostname // Set your domain here
     });
-}
+};
 
 export default generateTokenSetCookie;
-
-
-
